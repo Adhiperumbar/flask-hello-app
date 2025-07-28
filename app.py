3e907d6780a7ccd9dd3f3356051ce3bd909ca196
Adhiperumbar <adhishriperumbaru@gmail.com>
2025-07-28 21:11:07 +0530

    Implementing Form METHODS like GET,POST

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
-@app.route('/hello/<name>')
-def hello(name):
-    return f'Hello,{name}! Welcome to Flask'
+def form():
+    return render_template('form.html')
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        message=request.form['message']
+        return render_template('thankyou.html',name=name,message=message)
 if __name__=='__main__':
     print("My first flask app")
     print("App created successfully")
